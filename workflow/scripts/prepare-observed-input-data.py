20ff4b01ff1dcc1bc5d525f23a40cd9c8067c989
simonmoulds <sim.moulds@gmail.com>
2022-09-29 12:21:02 +0100

    changing cli

--- workflow/scripts/prepare-observed-input-data.py
+++ workflow/scripts/prepare-observed-input-data.py
@@ -5,10 +5,10 @@ import click
 from utils import _observed_preprocessor
 
 @click.command()
-@click.option('-i', '--input', 'config', default='config.yml', help='Input YAML configuration file')
+@click.option('-i', '--inputdir', default='.', help='Input YAML configuration file')
 @click.option('-o', '--outputdir', default='.', help='Output directory')
-def main(config, outputdir):
-    _observed_preprocessor(config, outputdir)
+def main(inputdir, outputdir):
+    _observed_preprocessor(inputdir, outputdir)
 
 if __name__ == '__main__':
     main()
