1ae26339bb43249d07698523cee776ce6f505b99
simonmoulds <sim.moulds@gmail.com>
2022-09-29 11:37:46 +0100

    bug fix

--- workflow/scripts/prepare-modelled-input-data.py
+++ workflow/scripts/prepare-modelled-input-data.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-# import click
+import click
 
 from utils import _ensemble_preprocessor
 
-_ensemble_preprocessor(snakemake.config, snakemake.params['outputdir'])
-# @click.command()
-# @click.option('-i', '--input', 'config', default='config.yml', help='Input YAML configuration file')
-# @click.option('-o', '--outputdir', default='.', help='Output directory')
-# def main(config, outputdir):
-#     _ensemble_preprocessor(config, outputdir)
+# _ensemble_preprocessor(snakemake.config, snakemake.params['outputdir'])
+@click.command()
+@click.option('-i', '--input', 'config', default='config.yml', help='Input YAML configuration file')
+@click.option('-o', '--outputdir', default='.', help='Output directory')
+def main(config, outputdir):
+    _ensemble_preprocessor(config, outputdir)
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
